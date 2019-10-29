8b85c2f633b1e8d0f60a1e6876710207f10ac8be
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2019-10-29 11:00:07 +0100

    Use variables for deployment configuration

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,15 @@ INPUT_FILE=template.yaml
 OUTPUT_FILE=template-output.yaml
 REGION=
 STACK_NAME=build-status-bot
+SLACK_WEBHOOK_URL=
+CODE_PIPELINE_NAME=
 
 aws cloudformation package --template-file $INPUT_FILE \
                            --s3-bucket $S3_BUCKET \
                            --output-template-file $OUTPUT_FILE
 aws cloudformation deploy --template-file $OUTPUT_FILE \
                           --stack-name $STACK_NAME \
-                          --parameter-overrides SlackWebhookURL= \
-                                                CodePipelineName= \
+                          --parameter-overrides SlackWebhookURL=$SLACK_WEBHOOK_URL \
+                                                CodePipelineName=$CODE_PIPELINE_NAME \
                           --capabilities CAPABILITY_NAMED_IAM \
                           --region $REGION \
